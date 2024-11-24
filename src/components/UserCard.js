1ffe5d109b852d96f09b9c8daf9c1f5ff7cad62e
bazanty <bazantyloffan@gmail.com>
2024-11-25 01:02:45 +0300

    complete

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 
+function UserCard({ user }) {
+  if (!user) {
+    return <p>User data is not available.</p>;
+  }
 
-function UserCard({user}) {
   return (
     <article>
-        <h2>{user.name}</h2>
+      <h2>{user.name}</h2>
+      <Link to={`/profile/${user.id}`}>View profile</Link>
     </article>
   );
-};
+}
 
 export default UserCard;
